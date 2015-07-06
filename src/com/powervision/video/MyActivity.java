9baf037815822dc55f542b5f9c9f8fde490e4f9e
FinalReality <FinalRealityWei@gmail.com>
2015-07-06 20:03:59 +0800

    Decode class OK

--- src/com/powervision/video/MyActivity.java
+++ src/com/powervision/video/MyActivity.java
@@ -2,25 +2,75 @@ package com.powervision.video;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.powervision.video.media.DataExtractor;
-import com.powervision.video.media.ExtractorFactory;
-import com.powervision.video.media.IDataExtractor;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import com.powervision.video.media.codec.*;
+import com.powervision.video.media.extractor.DataExtractor;
+import com.powervision.video.media.extractor.ExtractorFactory;
+import com.powervision.video.media.extractor.IDataExtractor;
 
-public class MyActivity extends Activity
+public class MyActivity extends Activity implements OnFrameProcessedListener, SurfaceHolder.Callback
 {
+    final boolean HASSURFACE = true;
+    public Surface mSurface = null;
+    int mWidth = 1280;
+    int mHeight = 720;
+    ICodec codec;
+    IDataExtractor extractor;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        SurfaceView sv = new SurfaceView(this);
+        sv.getHolder().addCallback(this);
+        setContentView(sv);
 
         //IDataExtractor extractor = ExtractorFactory.createFileDataExtractor("/storage/emulated/0/test.h264");
-        IDataExtractor extractor = ExtractorFactory.createFileDataExtractor("/storage/sdcard0/test.h264");
+        extractor = ExtractorFactory.createFileDataExtractor("/mnt/sdcard/test.h264");
         extractor.openDataExtractor();
         extractor.start();
-        do {
 
-        } while(extractor.getStatus() != DataExtractor.DATA_EXTRACTOR_STATUS_OK);
+    }
+
+    @Override
+    public void onFrameProcessed(byte[] processedFrame) {
+        Log.i("TEST", "MyActivity onFrameProcessed");
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+        if(HASSURFACE) {
+            mSurface = surfaceHolder.getSurface();
+        } else {
+            mSurface = null;
+        }
+
+        CodecParam param = new CodecParam();
+        param.extractor = extractor;
+        //param.surface = mSurface;
+        param.width = 1280;
+        param.height = 720;
+        CodecParam.codecType = Codec.CODEC_TYPE_MEDIACODEC;
+        codec = CodecFactory.createCodec(param);
+        codec.asObject().setOnFrameProcessedListener(this);
+
+        codec.asObject().initCodec(null);
+        codec.asObject().openCodec();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
     }
 }
--- src/com/powervision/video/media/DataExtractor.java
+++
@@ -1,9 +0,0 @@
-package com.powervision.video.media;
-
-/**
- * Created by liwei on 15-7-3.
- */
-public class DataExtractor {
-    public static final int DATA_EXTRACTOR_STATUS_OK = 1;
-    public static final int DATA_EXTRACTOR_STATUS_FAILED = 0;
-}
